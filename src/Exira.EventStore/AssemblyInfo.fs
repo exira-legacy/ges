a8777fc2f2f625ea3b83d5efecc1963133a9e2c0
Team City <info@exira.com>
2015-12-28 11:07:22 +0000

    Bump version to 0.7.15

--- src/Exira.EventStore/AssemblyInfo.fs
+++ src/Exira.EventStore/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.EventStore")>]
 [<assembly: AssemblyProductAttribute("Exira.EventStore")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.EventStore is an F# implementation for EventStore (https://geteventstore.com/)")>]
-[<assembly: AssemblyVersionAttribute("0.6.14")>]
-[<assembly: AssemblyFileVersionAttribute("0.6.14")>]
-[<assembly: AssemblyMetadataAttribute("githash","df7b34e897f30729c2b803436f1f7161efe65cb6")>]
+[<assembly: AssemblyVersionAttribute("0.7.15")>]
+[<assembly: AssemblyFileVersionAttribute("0.7.15")>]
+[<assembly: AssemblyMetadataAttribute("githash","1aafaa480dbcab9ea943a036c39d921abae35da0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.6.14"
+    let [<Literal>] Version = "0.7.15"
